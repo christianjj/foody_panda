d4b88967e370bdeb174ac2710e095106c75bbcc2
chjimenez <cfjimenez@snrshopping.com>
2025-01-15 16:01:36 +0800

    section 4.20

--- lib/view/category/all_categories.dart
+++ lib/view/category/all_categories.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foody/common/reusable_text.dart';
+import 'package:foody/constants/constants.dart';
+
+import '../../common/app_style.dart';
+import '../../constants/uidata.dart';
 
 class AllCategories extends StatelessWidget {
   const AllCategories({super.key});
@@ -7,7 +13,37 @@ class AllCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("All categories"),
+        elevation: 0,
+        backgroundColor: kOffWhite,
+        title: ReusableText(
+          text: "All categories",
+          style: appStyle(12, kGray, FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 12.w, top: 10.h),
+        height: height,
+        child: ListView(
+            scrollDirection: Axis.vertical,
+            children: List.generate(categories.length, (i) {
+              var category = categories[i];
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 18.r,
+                  backgroundColor: kGrayLight,
+                  child:
+                      Image.network(category["imageUrl"], fit: BoxFit.contain),
+                ),
+                title: ReusableText(
+                    text: category["title"],
+                    style: appStyle(12, kGray, FontWeight.normal)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: kGray,
+                  size: 15.r,
+                ),
+              );
+            })),
       ),
     );
   }
