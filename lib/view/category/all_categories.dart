5388ded0f28d5ab57b3a11512d6e829c2b17d54e
chjimenez <cfjimenez@snrshopping.com>
2025-01-15 16:18:38 +0800

    section 4.21

--- lib/view/category/all_categories.dart
+++ lib/view/category/all_categories.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foody/common/back_ground_contatiner.dart';
 import 'package:foody/common/reusable_text.dart';
 import 'package:foody/constants/constants.dart';
 
@@ -20,30 +21,33 @@ class AllCategories extends StatelessWidget {
           style: appStyle(12, kGray, FontWeight.w600),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.only(left: 12.w, top: 10.h),
-        height: height,
-        child: ListView(
-            scrollDirection: Axis.vertical,
-            children: List.generate(categories.length, (i) {
-              var category = categories[i];
-              return ListTile(
-                leading: CircleAvatar(
-                  radius: 18.r,
-                  backgroundColor: kGrayLight,
-                  child:
-                      Image.network(category["imageUrl"], fit: BoxFit.contain),
-                ),
-                title: ReusableText(
-                    text: category["title"],
-                    style: appStyle(12, kGray, FontWeight.normal)),
-                trailing: Icon(
-                  Icons.arrow_forward_ios_rounded,
-                  color: kGray,
-                  size: 15.r,
-                ),
-              );
-            })),
+      body: BackGroundContainer(
+        color: Colors.white,
+        child: Container(
+          padding: EdgeInsets.only(left: 12.w, top: 10.h),
+          height: height,
+          child: ListView(
+              scrollDirection: Axis.vertical,
+              children: List.generate(categories.length, (i) {
+                var category = categories[i];
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 18.r,
+                    backgroundColor: kGrayLight,
+                    child:
+                        Image.network(category["imageUrl"], fit: BoxFit.contain),
+                  ),
+                  title: ReusableText(
+                      text: category["title"],
+                      style: appStyle(12, kGray, FontWeight.normal)),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: kGray,
+                    size: 15.r,
+                  ),
+                );
+              })),
+        ),
       ),
     );
   }
