921b9737ed069dbe2f00354c8fe6acc70a17223f
chjimenez <cfjimenez@snrshopping.com>
2025-01-22 08:19:41 +0800

    section 30

--- lib/view/home/widgets/restaurant_tile.dart
+++ lib/view/home/widgets/restaurant_tile.dart
@@ -4,25 +4,23 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foody/common/app_style.dart';
 import 'package:foody/common/reusable_text.dart';
 import 'package:foody/constants/constants.dart';
-import 'package:foody/constants/uidata.dart';
 
 class RestaurantTile extends StatelessWidget {
-  const RestaurantTile({super.key, this.restaurant});
+  const RestaurantTile({super.key, required this.restaurant});
 
   final dynamic restaurant;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(onTap: () {}, child: Stack(
-      clipBehavior: Clip.hardEdge,
-        children: [
-          Container(margin: EdgeInsets.only(bottom: 8),
-          height: 70.h,
+    return GestureDetector(
+        onTap: () {},
+        child: Stack(clipBehavior: Clip.hardEdge, children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 8),
+            height: 70.h,
             width: width,
             decoration: BoxDecoration(
-            color: kOffWhite,
-              borderRadius: BorderRadius.circular(9.r)
-            ),
+                color: kOffWhite, borderRadius: BorderRadius.circular(9.r)),
             child: Container(
               padding: EdgeInsets.all(4.r),
               child: Row(
@@ -33,41 +31,47 @@ class RestaurantTile extends StatelessWidget {
                     child: Stack(
                       children: [
                         SizedBox(
-                        width: 70.w,
-                        height : 70.h,
-                          child: Image.network(restaurant["imageUrl"], fit: BoxFit.cover,),
+                          width: 70.w,
+                          height: 70.h,
+                          child: Image.network(
+                            restaurant["imageUrl"],
+                            fit: BoxFit.cover,
+                          ),
                         ),
                         Positioned(
                           bottom: 0,
-                            child: Container(
-                              padding: EdgeInsets.only(left: 6.w, bottom: 2.h),
-                              color: kGray.withOpacity(0.6),
-                              height: 16.h,
-                              width: width,
-                              child: RatingBarIndicator(rating: 5,itemCount: 5, itemBuilder: (context, i) =>
-                               Icon(Icons.star, color: kSecondary),
-                                  itemSize: 15.h
-                              ),
-
-                            ),
+                          child: Container(
+                            padding: EdgeInsets.only(left: 6.w, bottom: 2.h),
+                            color: kGray.withOpacity(0.6),
+                            height: 16.h,
+                            width: width,
+                            child: RatingBarIndicator(
+                                rating: 5,
+                                itemCount: 5,
+                                itemBuilder: (context, i) =>
+                                    Icon(Icons.star, color: kSecondary),
+                                itemSize: 15.h),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(width: 10.w,),
+                  SizedBox(
+                    width: 10.w,
+                  ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      ReusableText(text: restaurant['title'], style: appStyle(11, kDark, FontWeight.w400))
+                      ReusableText(
+                          text: restaurant['title'],
+                          style: appStyle(11, kDark, FontWeight.w400))
                     ],
                   )
                 ],
               ),
             ),
           )
-        ]
-    )
-    );
+        ]));
   }
 }
