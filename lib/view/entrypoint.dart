31734a46185a49e9e95bcbe39ee72a97e40f166b
chjimenez <cfjimenez@snrshopping.com>
2025-01-22 08:19:41 +0800

    section 30

--- lib/view/entrypoint.dart
+++ lib/view/entrypoint.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_vector_icons/flutter_vector_icons.dart';
-import 'package:foody/constants/constants.dart';
 import 'package:foody/view/bottom_navigation.dart';
 import 'package:foody/view/profile/profile_page.dart';
 import 'package:foody/view/search/search_page.dart';
--- lib/view/home/all_nearby_restaurants.dart
+++ lib/view/home/all_nearby_restaurants.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foody/constants/uidata.dart';
+import 'package:foody/view/home/widgets/restaurant_tile.dart';
 
 import '../../common/app_style.dart';
 import '../../common/reusable_text.dart';
@@ -12,14 +15,19 @@ class AllNearbyRestaurants extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.3,
-          backgroundColor: kOffWhite,
-          title: ReusableText(
-            text: "Nearby Restaurants",
-            style: appStyle(13, kGray, FontWeight.w600),
-          ),
+        backgroundColor: kOffWhite,
+        title: ReusableText(
+          text: "Nearby Restaurants",
+          style: appStyle(13, kGray, FontWeight.w600),
+        ),
       ),
-      body: const Center(
-        child: Text("All Nearby Restaurants"),
+      body: Padding(
+        padding: EdgeInsets.all(12.w),
+        child: ListView(
+            children: List.generate(restaurants.length, (i) {
+          var restaurant = restaurants[i];
+          return RestaurantTile(restaurant: restaurant);
+        })),
       ),
     );
   }
